ca1a148ea308c41c8a74af3dee466a1368c93ca5
thanhtra <19522370@gm.uit.edu.vn>
2023-10-14 15:28:01 +0700

    Create Edit and Delete Channel

---
+++ components/server/server-member.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Member, MemberRole, Profile, Server } from "@prisma/client";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { UserAvatar } from "@/components/user-avatar";
+
+interface ServerMemberProps {
+  member: Member & { profile: Profile };
+  server: Server;
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="w-4 h-4 ml-2 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="w-4 h-4 ml-2 text-rose-500" />,
+};
+
+export const ServerMember = ({ member, server }: ServerMemberProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const icon = roleIconMap[member.role];
+
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <UserAvatar
+        src={member.profile.imageUrl}
+        className="w-8 h-8 md:w-8 md:h-8"
+      />
+      <p
+        className={cn(
+          "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dar:group-hover:text-zinc-300 transition",
+          params?.memberId === member.id &&
+            "text-primary dark:text-zinc-200 dar:group-hover:text-white"
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
+};
